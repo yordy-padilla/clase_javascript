029642802b354496d8603f62c11881f3106d9e67
Yordy Padilla <yjrpadilla@gmail.com>
2023-05-24 22:28:14 -0500

    segundo commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-alert('Hola Mundo');
+alert('Hola')
+
+//consumir api
+const obtener_personaje = async () => {
+    try{
+        const resp = await fetch("https://rickandmortyapi.com/api/character")
+        const data = await resp.json()
+        const results= data.results
+        console.log(results)  
+    }catch(error){
+        console.log(error)
+    }
+}
+
+obtener_personaje()
